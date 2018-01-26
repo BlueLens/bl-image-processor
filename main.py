378fa007feff641ebe2bbaac2495890cf75df1f6
Youngbok Yoon <master@bluehack.net>
2018-01-26 22:31:41 +0900

    Fixed duplicated downloading image problem

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ HEALTH_CHECK_TIME = 300
 TMP_MOBILE_IMG = 'tmp_mobile_full.jpg'
 TMP_MOBILE_THUMB_IMG = 'tmp_mobile_thumb.jpg'
 
+TMP_IMG = 'tmp.jpg'
+
 SPAWN_ID = os.environ['SPAWN_ID']
 REDIS_SERVER = os.environ['REDIS_SERVER']
 REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
@@ -60,7 +62,27 @@ def process_image(p_data):
 
   make_mobile_images(product)
 
-def make_mobile_image(image_name, type, image_path):
+def convert_to_jpeg(format, image):
+  if 'gif' == format.lower():
+    log.debug('gif')
+    image.seek(0)
+    mypalette = image.getpalette()
+    image.putpalette(mypalette)
+    new_im = Image.new("RGBA", image.size)
+    new_im.paste(image)
+    new_im.save('foo' + '.png')
+
+    bg = Image.new("RGB", image.size, (255,255,255))
+    bg.paste(new_im, (0,0), new_im)
+    return bg
+
+  elif 'png' == format.lower():
+    return image.convert('RGB')
+  else:
+    return image
+
+
+def make_mobile_image(image_name, type, image):
 
   if type == 'thumb':
     basewidth = MOBILE_THUMBNAIL_WIDTH
@@ -68,9 +90,8 @@ def make_mobile_image(image_name, type, image_path):
     basewidth = MOBILE_FULL_WIDTH
 
   try:
-    # f = urllib.urlopen(urllib.quote(image_path.encode('utf8'), '/:'))
-    f = urllib.request.urlopen(image_path)
-    im = Image.open(f).convert('RGB')
+    format = image.format
+    im = convert_to_jpeg(format, image)
     wpercent = (basewidth / float(im.size[0]))
     hsize = int((float(im.size[1]) * float(wpercent)))
     im = im.resize((basewidth, hsize), Image.ANTIALIAS)
@@ -78,37 +99,81 @@ def make_mobile_image(image_name, type, image_path):
     file_url = save_mobile_image_to_storage(image_name, type)
     return file_url
   except Exception as e:
-    log.error('url open error')
+    log.error('make_bobile_image: ' + str(e))
     return None
 
+def download_image(path):
+  try:
+    f = urllib.request.urlopen(path)
+  except Exception as e:
+    log.error('download_image: ' + str(e))
+    return None
+  return f
+
+def set_product_is_unavailable(product):
+  try:
+    p = {}
+    p['is_available'] = False
+    product_api.update_product_by_id(str(product['_id']), p)
+  except Exception as e:
+    log.error(str(e))
+
 def make_mobile_images(product):
-  full_image = make_mobile_image(str(product['_id']), 'full', product['main_image'])
-  if full_image == None:
+  product_id = str(product['_id'])
+
+  file = download_image(product.get('main_image'))
+  if file is None:
+    set_product_is_unavailable(product)
     return
-  thumb_image = make_mobile_image(str(product['_id']), 'thumb', product['main_image'])
-  if thumb_image == None:
+
+  try:
+    image = Image.open(file)
+  except Exception as e:
+    log.error('make_mobile_images: ' + str(e))
+    return
+
+  full_image = make_mobile_image(product_id, 'full', image)
+  if full_image is None:
+    set_product_is_unavailable(product)
+    return
+
+  thumb_image = make_mobile_image(product_id, 'thumb', image)
+  if thumb_image is None:
+    set_product_is_unavailable(product)
     return
 
   sub_images = []
-  for sub_img in product['sub_images']:
-    sub_image = make_mobile_image(str(uuid.uuid4()), 'sub', sub_img)
-    if sub_image is not None:
-      sub_images.append(sub_image)
+  if product.get('sub_images, None') != None:
+    for sub_img in product.get('sub_images', None):
+      sub_image = make_mobile_image(str(uuid.uuid4()), 'sub', sub_img)
+      if sub_image is not None:
+        sub_images.append(sub_image)
 
   product['main_image_mobile_full'] = full_image
   product['main_image_mobile_thumb'] = thumb_image
   product['sub_images_mobile'] = sub_images
   product['is_processed']= True
+  product['is_available']= True
   update_product_to_db(product)
+
+def delete_product_from_db(product_id):
+  try:
+    product_api.delete_product(product_id)
+  except Exception as e:
+    log.error(str(e))
 
 def save_mobile_image_to_storage(name, path):
   log.debug('save_mobile_image_to_storage')
-  file = name + '.jpg'
-  key = os.path.join(RELEASE_MODE, 'mobile', path, name + '.jpg')
-  is_public = True
-  file_url = storage.upload_file_to_bucket(AWS_MOBILE_IMAGE_BUCKET, file, key, is_public=is_public)
-  #log.info(file_url)
-  return file_url
+  try:
+    file = name + '.jpg'
+    key = os.path.join(RELEASE_MODE, 'mobile', path, name + '.jpg')
+    is_public = True
+    file_url = storage.upload_file_to_bucket(AWS_MOBILE_IMAGE_BUCKET, file, key, is_public=is_public)
+    #log.info(file_url)
+    return file_url
+  except Exception as e:
+    log.error('save_mobile_image_to_storage: ' + str(e))
+    return None
 
 def update_product_to_db(product):
   log.debug('update_product_to_db')
@@ -132,7 +197,8 @@ def delete_pod():
 
   data = {}
   data['namespace'] = RELEASE_MODE
-  data['id'] = SPAWN_ID
+  data['key'] = 'SPAWN_ID'
+  data['value'] = SPAWN_ID
   spawn = spawning_pool.SpawningPool()
   spawn.setServerUrl(REDIS_SERVER)
   spawn.setServerPassword(REDIS_PASSWORD)
@@ -152,15 +218,15 @@ def dispatch_job(rconn):
     elapsed_time = time.time() - start_time
     log.info('image-processing time: ' + str(elapsed_time))
 
-    if count > MAX_PROCESS_NUM:
-      delete_pod()
+    #if count > MAX_PROCESS_NUM:
+    #  delete_pod()
 
     global  heart_bit
     heart_bit = True
 
 if __name__ == '__main__':
   try:
-    log.info('Start bl-image-processor:7')
+    log.info('Start bl-image-processor:3')
     dispatch_job(rconn)
   except Exception as e:
     log.error(str(e))
